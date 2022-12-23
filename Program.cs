76649f667f7b89a05933bc8c352e95df3860e064
TeoVincent <artur.wincenciak@gmail.com>
2022-12-23 16:41:14 +0100

    Passing an argument by reference

--- Program.cs
+++ Program.cs
@@ -1,9 +1,34 @@
 ﻿Console.WriteLine("Alright ref!");
 
-Console.WriteLine("Passing an argument by reference.");
-PassingAnArgumentByReferenceJoy();
+/*
+ * Note
+ *
+ * Don't confuse the concept of passing by reference with the concept of reference types.
+ * The two concepts are not the same.A method parameter can be modified by ref regardless
+ * of whether it is a value type or a reference type. There is no boxing of a value type
+ * when it is passed by reference.
+ */
 
-void PassingAnArgumentByReferenceJoy()
+Console.WriteLine("Passing an argument by reference.");
+
+var globalInt = 100;
+
+ArgIntMethod(globalInt);
+Console.WriteLine($"#1: {nameof(globalInt)} = {globalInt}"); // 100
+
+ArgRefIntMethod(ref globalInt);
+Console.WriteLine($"#2: {nameof(globalInt)} = {globalInt}"); // 200
+
+void ArgIntMethod(int argInt)
 {
-    
+    Console.WriteLine($"{nameof(ArgIntMethod)} #1: {nameof(argInt)} = {argInt}"); // 100
+    argInt += 100;
+    Console.WriteLine($"{nameof(ArgIntMethod)} #2: {nameof(argInt)} = {argInt}"); // 200
+}
+
+void ArgRefIntMethod(ref int refArgInt)
+{
+    Console.WriteLine($"{nameof(ArgRefIntMethod)} #1: {nameof(refArgInt)} = {refArgInt}"); // 100
+    refArgInt += 100;
+    Console.WriteLine($"{nameof(ArgRefIntMethod)} #2: {nameof(refArgInt)} = {refArgInt}"); // 200
 }
